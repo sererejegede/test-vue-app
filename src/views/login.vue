fdd164732e98f901a3679dbc7b35ab3779b543f5
sererejegede <sererejegede@gmail.com>
2021-02-24 14:08:12 +0100

    initial commit

---
+++ src/views/login.vue
@@ -0,0 +1,63 @@
+<template>
+  <div id="login">
+    <h1>Login</h1>
+    <input
+      type="text"
+      name="username"
+      v-model="input.username"
+      placeholder="Enter Username"
+    />
+    <br />
+    <br />
+    <input
+      type="password"
+      name="password"
+      v-model="input.password"
+      placeholder="Enter Password"
+    />
+    <br /><br />
+    <button type="button" v-on:click="login()">Login</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Login",
+  data() {
+    return {
+      input: {
+        username: "",
+        password: "",
+      },
+    };
+  },
+  methods: {
+    login() {
+      if (this.input.username != "" && this.input.password != "") {
+        if (
+          this.input.username == this.$parent.playAccount.username &&
+          this.input.password == this.$parent.playAccount.password
+        ) {
+          this.$emit("authenticated", true);
+          this.$router.replace({ name: "welcome" });
+        } else {
+          console.log("The username and password is incorrect");
+        }
+      } else {
+        console.log("A username and password must be entered");
+      }
+    },
+  },
+};
+</script>
+
+<style scoped>
+#login {
+  width: 80%;
+  border: 1px solid #cccccc;
+  background-color: blue;
+  margin: auto;
+  margin-top: 200px;
+  padding: 20px;
+}
+</style>
---
+++ src/views/welcome.vue
@@ -0,0 +1,28 @@
+  
+<template>
+  <div id="welcome">
+    <h1>Login Area</h1>
+    <p>
+      Welcome Here!!! <br />
+      You are logged in!!!
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Welcome",
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style scoped>
+#welcome {
+  background-color: purple;
+  border: 1px solid #2c3e50;
+  padding: 20px;
+  margin-top: 10px;
+}
+</style>
